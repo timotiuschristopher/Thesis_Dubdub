ece7f1460ff1d1f5e32ce03dc685a3cc09c3bab6
foxtrot <timotiuschristopher.tct@gmail.com>
2022-04-06 22:43:32 +0800

    Added Graph routing, modified line 65-70 in server.js

---
+++ routes/graph.js
@@ -0,0 +1,13 @@
+"use strict";
+const express = require("express");
+let router = express.Router();
+
+// let reqPath = path.join(__dirname,'../graph.htm')
+
+router
+.route('')
+.get(function (req, res) {
+  res.sendFile('../graph.htm') //__dirname + 'C:/Users/Debby/Documents/thesistrial/thesistrial/graph.htm'
+})
+
+module.exports = router;
