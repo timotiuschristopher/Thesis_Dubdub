00f9688a1c095426d236e71b64ee469a0a7c5f7e
foxtrot <timotiuschristopher.tct@gmail.com>
2022-04-14 14:56:12 +0800

    Before using JSON

--- services/csv2jsonconverter.js
+++ services/csv2jsonconverter.js
@@ -1,15 +1,16 @@
-const csvFilePath='../209-SVT.csv'
-const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
-    /**
-     * [
-     * 	{a:"1", b:"2", c:"3"},
-     * 	{a:"4", b:"5". c:"6"}
-     * ]
-     */ 
-})
+// const csvFilePath='../209-SVT.csv'
+// const csv=require('csvtojson')
 
-const jsonArray=await csv().fromFile(csvFilePath);
+// export function csv()
+// .fromFile(csvFilePath)
+// .then((jsonObj)=>{
+//     console.log(jsonObj);
+//     /**
+//      * [
+//      * 	{a:"1", b:"2", c:"3"},
+//      * 	{a:"4", b:"5". c:"6"}
+//      * ]
+//      */ 
+// })
+
+// const jsonArray=await csv().fromFile(csvFilePath);
