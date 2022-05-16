d2e64b4cff0f12ad6f1af9176a93628b4753b563
foxtrot <timotiuschristopher.tct@gmail.com>
2022-05-16 21:54:10 +0800

    change dir for graph.htm 2

--- routes/graph.js
+++ routes/graph.js
@@ -1,13 +1,14 @@
 "use strict";
 const express = require("express");
 let router = express.Router();
+var path = require("path");
 
 // let reqPath = path.join(__dirname,'../graph.htm')
 
 router
 .route('')
 .get(function (req, res) {
-  res.sendFile(express.static(__dirname + '../graph.htm')) //__dirname + 'C:/Users/Debby/Documents/thesistrial/thesistrial/graph.htm'
+  res.sendFile(path.join(__dirname + '/../index.htm')) //__dirname + 'C:/Users/Debby/Documents/thesistrial/thesistrial/graph.htm'
 })
 
 module.exports = router;
